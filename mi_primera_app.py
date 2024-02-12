e0351de89a89e03d284e0b53c23834dbf9493f7a
spalominor <159827322+spalominor@users.noreply.github.com>
2024-02-12 17:01:56 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+# Título y autor
+st.title("Mi primera app")
+st.write("Esta app fue elaborada por Samuel Palomino")
+
+# Preguntar al usuario su nombre
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+
+# Saludar al usuario
+if nombre_usuario:
+    st.write(f"Hola {nombre_usuario}, te doy la bienvenida a mi primera app.")
